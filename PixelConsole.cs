5fb88c7259757a7f3188fe386400274a8273823f
DIVE <Dima16062004@rambler.ru>
2024-11-08 18:09:06 +0300

    Добавьте файлы проекта.

---
+++ PixelConsole.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2dtest
+{
+    internal class PixelConsole
+    {
+        const bool DEBUG = true;
+
+        // ==============================================
+
+
+        public static void PrintPixel(int w, int h)
+        {
+
+            if (w > 120 || h > 30) throw new ArgumentException("Height or Width out of bounds");
+           // bool[] matrix = new bool[10];
+            
+            
+            
+            
+
+            Console.SetCursorPosition(0, h-1);
+
+            for (int i = 0; i < w - 1; i++)
+            {
+                if (DEBUG) Console.Write('.');
+                else Console.Write((Char)32);
+            }
+            Console.Write("\u25a0");
+
+        }
+
+        public static void PrintLine(bool[] massiv)
+        {
+            if (massiv.Length > 120) throw new ArgumentException("Height or Width out of bounds");
+            foreach (bool i in massiv)
+            {
+                if (i == true) Console.Write("\u25a0");
+                else if (DEBUG) Console.Write('.');
+                else Console.Write((Char)32);
+            }
+
+        }
+        public static void PrintLine(int[] massiv)
+        {
+            if (massiv.Length > 120) throw new ArgumentException("Height or Width out of bounds");
+            foreach (int i in massiv)
+            {
+                if (i == 1) Console.Write("\u25a0");
+                else if (DEBUG) Console.Write('.');
+                else Console.Write((Char)32);
+            }
+
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,46 @@
+﻿using System.Runtime.ConstrainedExecution;
+using System.Runtime.InteropServices;
+using System.Runtime.Serialization.Formatters;
+using System;   
+using System.Text;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;
+using draw = _2dtest.PixelConsole;
+namespace _2dtest
+{
+    internal class Program
+    {
+
+
+
+        static void Main(string[] args)
+        {
+            /*
+             Итак, можно использовать символ квадрата: \u25a0
+             Разрешение экрана 120x30
+             Я предлагаю просто-напросто передавать буллевый массив
+             И класс будет просто напросто построчно рисовать дисплей. 
+             Иначе, класс PixelConsole будет ну типа планировщиком, а в основном коде будем уже писать пиксели
+
+            
+
+            */
+            int width = 120;
+            int height = 4;
+
+            bool[] matrixB = { true, true, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, true, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false };
+            int[] matrixI = {1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+            Console.Title = "It's 2DTest Console Graphics";
+
+            while (true)
+            {
+                draw.PrintLine(matrixB);
+                //MessageBox.Show("Hola");
+                
+
+                Console.ReadKey();
+            }
+
+        }
+    }
+}
